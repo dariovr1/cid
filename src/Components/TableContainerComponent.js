9ce708e7595e24909d4ffaa5261e238c178ae186
Antonino Dario Amato <dario.amato@finconsgroup.local>
2021-11-29 10:48:44 +0100

    finishing table Container

--- src/Components/TableContainerComponent.js
+++ src/Components/TableContainerComponent.js
@@ -6,17 +6,30 @@ import SaveIcon from '@mui/icons-material/Save';
 
 const TableContainerContainer = (props) => {
 
-    const [toggle, setToggle] = useState(false);
-    const [inputvalue, setInputValue] = useState(34);
+    const [togglemin, setMinToggle] = useState(false);
+    const [togglemax, setMaxToggle] = useState(false);
+    const [inputMinvalue, setInputMinValue] = useState(28);
+    const [inputMaxvalue, setInputMaxValue] = useState(34);
 
-    const handleClick = (e) => {
-        console.log("handleclick test icon ", !toggle);
-        setToggle(!toggle);
+
+    const handleMinClick = (e) => {
+        console.log("handleclick min test icon ", !togglemin);
+        setMinToggle(!togglemin);
     };
 
-    const handleCLickValue = (e) => {
+    const handleCLickMinValue = (e) => {
+        console.log("handleClickValue min", e);
+        setInputMinValue(e);
+    };
+
+    const handleMaxClick = (e) => {
+        console.log("handleclick test icon ", !togglemax);
+        setMaxToggle(!togglemax);
+    };
+
+    const handleCLickMaxValue = (e) => {
         console.log("handleClickValue ", e);
-        setInputValue(e);
+        setInputMaxValue(e);
     };
 
     return (
@@ -25,19 +38,36 @@ const TableContainerContainer = (props) => {
             <div>
                 <h2>LU44 Maker</h2>
             </div>
-            <div style={{display : 'flex', 'alignItems': 'center', gap: '20px'}}>
-                {(!toggle) ? <TextField  id="outlined-required" disabled label="Reject Limit" defaultValue={inputvalue} />  :  <TextField
+                <div style={{display : 'flex', 'alignItems': 'center', flexDirection : 'column', gap: '20px'}}>
+                    <div class="rejCont" style={{display: 'flex', 'alignItems': 'center', gap: '10px'}}>
+                    {(!togglemin) ? <TextField  id="outlined-required" disabled label="Reject Min Limit" defaultValue={inputMinvalue} />  :  <TextField
+                    id="outlined-number"
+                    label="Number"
+                    onClick={handleCLickMinValue}
+                    type="number"
+                    InputLabelProps={{
+                    shrink: true,
+                    }}
+                />}
+                    {(togglemin) ? <SaveIcon onClick={handleMinClick} /> : <EditIcon onClick={handleMinClick} />}
+                    </div>
+               
+                    <div class="rejCont" style={{display: 'flex', 'alignItems': 'center', gap: '10px'}}>
+                {(!togglemax) ? <TextField  id="outlined-required" disabled label="Reject Max Limit" defaultValue={inputMaxvalue} />  :  <TextField
                 id="outlined-number"
                 label="Number"
-                onClick={handleCLickValue}
+                onClick={handleCLickMaxValue}
                 type="number"
                 InputLabelProps={{
-                  shrink: true,
+                shrink: true,
                 }}
-              />}
-                {(toggle) ? <SaveIcon onClick={handleClick} /> : <EditIcon onClick={handleClick} />}
-            </div>
-          </div>
+            />}
+                {(togglemax) ? <SaveIcon onClick={handleMaxClick} /> : <EditIcon onClick={handleMaxClick} />}
+                    </div>
+                </div>
+                <br/>
+         <h8 style={{color: '#004f90'}}>SHOW DETAILS</h8>
+                </div>
           <TableUI />
        </div>
     )
